4507e45220b0e99a78b1f39508906093c13d43f9
robthecornall@gmail.com <rcornall@suf.ca>
2017-01-02 02:20:57 -0800

    basic main page + button to go to blank activity.

--- app/src/main/java/com/robthecornallgmail/memarket/MainActivity.java
+++ app/src/main/java/com/robthecornallgmail/memarket/MainActivity.java
@@ -6,9 +6,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View.OnClickListener;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,21 +19,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+
+
         });
+        // Locate the button in activity_main.xml
+        Button button = (Button) findViewById(R.id.startButton);
+        // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                // Start NewActivity.class
+                Intent myIntent = new Intent(MainActivity.this,
+                MenuActivity.class);
+                startActivity(myIntent);
+            }
+        });
+
 
     // Example of a call to a native method
     TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());
+    //    tv.setText(stringFromJNI());
     }
 
     @Override
